a8c539741759600c7bf17f20bed9be90ed180d0b
Simon Asp <simpasp@gmail.com>
2016-09-08 15:32:57 +0200

    Fix

--- demo.py
+++ demo.py
@@ -6,13 +6,8 @@
 
     Updated by Ola Ringdahl 204-09-11
     """
-from serverIp2 import *
+from serverIp import *
 MRDS_URL = ip
-<<<<<<< HEAD
-
-bajs = 'bajs'
-=======
->>>>>>> a07df45557a8017060791b720204b3f8487d4f61
 
 import httplib, json, time
 from pprint import pprint
